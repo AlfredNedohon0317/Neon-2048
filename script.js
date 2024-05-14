cb3c4717e9e8a848caba949cca8fbd0003ca033c
AlfredNedohon0317 <alfrednedohon@gmail.com>
2024-05-14 09:59:18 -0400

    Project-1

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-//______________________________________const_____________________________________________
 const GRID_SIZE = 4;
 const EMPTY_CELL = 0;
-//______________________________________varible____________________________________________
+
+
 let gameBoard = [];
-let score = 0; 
-//______________________________________function____________________________________________
+let score = 0;
+
+
 function initializeGameBoard() {
     gameBoard = Array.from({ length: GRID_SIZE }, () => Array.from({ length: GRID_SIZE }, () => EMPTY_CELL));
 }
+
 
 function generateRandomTile() {
     let emptyCells = [];
@@ -19,11 +21,11 @@ function generateRandomTile() {
         }
     }
 
-    let randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
-   
-    let tileValue = Math.random() < 0.9 ? 2 : 4;
-
-    gameBoard[randomCell.row][randomCell.col] = tileValue;
+    if (emptyCells.length > 0) {
+        let randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+        let tileValue = Math.random() < 0.9 ? 2 : 4;
+        gameBoard[randomCell.row][randomCell.col] = tileValue;
+    }
 }
 
 function updateGameBoardDisplay() {
@@ -35,15 +37,24 @@ function updateGameBoardDisplay() {
             const tileValue = gameBoard[i][j];
             const tileElement = document.createElement('div');
             tileElement.className = 'tile';
-            tileElement.textContent = tileValue !== EMPTY_CELL ? tileValue : '';
+
+            if (tileValue !== EMPTY_CELL) {
+                const innerTileElement = document.createElement('div');
+                innerTileElement.className = 'inner-tile';
+                innerTileElement.textContent = tileValue;
+                tileElement.appendChild(innerTileElement);
+            }
+
             gameBoardElement.appendChild(tileElement);
         }
     }
 }
 
+
 function updateScoreDisplay(score) {
     document.getElementById('score').textContent = score;
 }
+
 
 function handleKeyPress(event) {
     switch (event.key) {
@@ -64,116 +75,109 @@ function handleKeyPress(event) {
 
 function moveTiles(direction) {
     let moved = false;
-    updateScoreDisplay(score)
 
     switch (direction) {
-                case 'up':
-                    for (let j = 0; j < GRID_SIZE; j++) {
-                        for (let i = 1; i < GRID_SIZE; i++) {
-                            if (gameBoard[i][j] !== EMPTY_CELL) {
-                                let row = i;
-                                while (row > 0 && gameBoard[row - 1][j] === EMPTY_CELL) {
-                                 
-                                    gameBoard[row - 1][j] = gameBoard[row][j];
-                                    gameBoard[row][j] = EMPTY_CELL;
-                                    row--;
-                                    moved = true;
-                                }
-                                if (row > 0 && gameBoard[row - 1][j] === gameBoard[row][j]) {
-                                   
-                                    gameBoard[row - 1][j] *= 2;
-                                    gameBoard[row][j] = EMPTY_CELL;
-                                    moved = true;
-                                }
-                            }
+        case 'up':
+            for (let j = 0; j < GRID_SIZE; j++) {
+                for (let i = 1; i < GRID_SIZE; i++) {
+                    if (gameBoard[i][j] !== EMPTY_CELL) {
+                        let row = i;
+                        while (row > 0 && gameBoard[row - 1][j] === EMPTY_CELL) {
+                            gameBoard[row - 1][j] = gameBoard[row][j];
+                            gameBoard[row][j] = EMPTY_CELL;
+                            row--;
+                            moved = true;
+                        }
+                        if (row > 0 && gameBoard[row - 1][j] === gameBoard[row][j]) {
+                            gameBoard[row - 1][j] *= 2;
+                            gameBoard[row][j] = EMPTY_CELL;
+                            score += gameBoard[row - 1][j];
+                            moved = true;
                         }
                     }
-                    break;
-                case 'down':
-                    for (let j = 0; j < GRID_SIZE; j++) {
-                        for (let i = GRID_SIZE - 2; i >= 0; i--) {
-                            if (gameBoard[i][j] !== EMPTY_CELL) {
-                                let row = i;
-                                while (row < GRID_SIZE - 1 && gameBoard[row + 1][j] === EMPTY_CELL) {
-                                   
-                                    gameBoard[row + 1][j] = gameBoard[row][j];
-                                    gameBoard[row][j] = EMPTY_CELL;
-                                    row++;
-                                    moved = true;
-                                }
-                                if (row < GRID_SIZE - 1 && gameBoard[row + 1][j] === gameBoard[row][j]) {
-                                   
-                                    gameBoard[row + 1][j] *= 2;
-                                    gameBoard[row][j] = EMPTY_CELL;
-                                    moved = true;
-                                }
-                            }
-                        }
-                    }
-                    break;
-                case 'left':
-                    for (let i = 0; i < GRID_SIZE; i++) {
-                        for (let j = 1; j < GRID_SIZE; j++) {
-                            if (gameBoard[i][j] !== EMPTY_CELL) {
-                                let col = j;
-                                while (col > 0 && gameBoard[i][col - 1] === EMPTY_CELL) {
-                                  
-                                    gameBoard[i][col - 1] = gameBoard[i][col];
-                                    gameBoard[i][col] = EMPTY_CELL;
-                                    col--;
-                                    moved = true;
-                                }
-                                if (col > 0 && gameBoard[i][col - 1] === gameBoard[i][col]) {
-                                   
-                                    gameBoard[i][col - 1] *= 2;
-                                    gameBoard[i][col] = EMPTY_CELL;
-                                    moved = true;
-                                }
-                            }
-                        }
-                    }
-                    break;
-                case 'right':
-                    for (let i = 0; i < GRID_SIZE; i++) {
-                        for (let j = GRID_SIZE - 2; j >= 0; j--) {
-                            if (gameBoard[i][j] !== EMPTY_CELL) {
-                                let col = j;
-                                while (col < GRID_SIZE - 1 && gameBoard[i][col + 1] === EMPTY_CELL) {
-                                   
-                                    gameBoard[i][col + 1] = gameBoard[i][col];
-                                    gameBoard[i][col] = EMPTY_CELL;
-                                    col++;
-                                    moved = true;
-                                }
-                                if (col < GRID_SIZE - 1 && gameBoard[i][col + 1] === gameBoard[i][col]) {
-                                    
-                                    gameBoard[i][col + 1] *= 2;
-                                    gameBoard[i][col] = EMPTY_CELL;
-                                    moved = true;
-                                }
-                            }
-                        }
-                    }
-                    break;
-            }
-            if (moved) {
-                generateRandomTile();
-                updateGameBoardDisplay();
-                console.log(score); 
-                updateScoreDisplay(score); 
-                console.log(score); 
-                if (checkGameOver()) {
-                    console.log('Game over!');
                 }
             }
+            break;
+        case 'down':
+            for (let j = 0; j < GRID_SIZE; j++) {
+                for (let i = GRID_SIZE - 2; i >= 0; i--) {
+                    if (gameBoard[i][j] !== EMPTY_CELL) {
+                        let row = i;
+                        while (row < GRID_SIZE - 1 && gameBoard[row + 1][j] === EMPTY_CELL) {
+                            gameBoard[row + 1][j] = gameBoard[row][j];
+                            gameBoard[row][j] = EMPTY_CELL;
+                            row++;
+                            moved = true;
+                        }
+                        if (row < GRID_SIZE - 1 && gameBoard[row + 1][j] === gameBoard[row][j]) {
+                            gameBoard[row + 1][j] *= 2;
+                            gameBoard[row][j] = EMPTY_CELL;
+                            score += gameBoard[row + 1][j];
+                            moved = true;
+                        }
+                    }
+                }
+            }
+            break;
+        case 'left':
+            for (let i = 0; i < GRID_SIZE; i++) {
+                for (let j = 1; j < GRID_SIZE; j++) {
+                    if (gameBoard[i][j] !== EMPTY_CELL) {
+                        let col = j;
+                        while (col > 0 && gameBoard[i][col - 1] === EMPTY_CELL) {
+                            gameBoard[i][col - 1] = gameBoard[i][col];
+                            gameBoard[i][col] = EMPTY_CELL;
+                            col--;
+                            moved = true;
+                        }
+                        if (col > 0 && gameBoard[i][col - 1] === gameBoard[i][col]) {
+                            gameBoard[i][col - 1] *= 2;
+                            gameBoard[i][col] = EMPTY_CELL;
+                            score += gameBoard[i][col - 1];
+                            moved = true;
+                        }
+                    }
+                }
+            }
+            break;
+        case 'right':
+            for (let i = 0; i < GRID_SIZE; i++) {
+                for (let j = GRID_SIZE - 2; j >= 0; j--) {
+                    if (gameBoard[i][j] !== EMPTY_CELL) {
+                        let col = j;
+                        while (col < GRID_SIZE - 1 && gameBoard[i][col + 1] === EMPTY_CELL) {
+                            gameBoard[i][col + 1] = gameBoard[i][col];
+                            gameBoard[i][col] = EMPTY_CELL;
+                            col++;
+                            moved = true;
+                        }
+                        if (col < GRID_SIZE - 1 && gameBoard[i][col + 1] === gameBoard[i][col]) {
+                            gameBoard[i][col + 1] *= 2;
+                            gameBoard[i][col] = EMPTY_CELL;
+                            score += gameBoard[i][col + 1];
+                            moved = true;
+                        }
+                    }
+                }
+            }
+            break;
+    }
+
+    if (moved) {
+        generateRandomTile();
+        updateGameBoardDisplay();
+        updateScoreDisplay(score);
+        if (checkGameOver()) {
+            console.log('Game over!');
         }
-    
-    
+    }
+}
+
 function checkGameOver() {
     for (let i = 0; i < GRID_SIZE; i++) {
         for (let j = 0; j < GRID_SIZE; j++) {
             if (gameBoard[i][j] === EMPTY_CELL) {
-                return false; 
+                return false;
             }
         }
     }
@@ -181,31 +185,28 @@ function checkGameOver() {
     for (let i = 0; i < GRID_SIZE; i++) {
         for (let j = 0; j < GRID_SIZE; j++) {
             if (j < GRID_SIZE - 1 && gameBoard[i][j] === gameBoard[i][j + 1]) {
-                return false; 
+                return false;
             }
             if (i < GRID_SIZE - 1 && gameBoard[i][j] === gameBoard[i + 1][j]) {
-                return false; 
+                return false;
             }
         }
     }
 
-    return true; 
+    return true;
 }
 
 function startGame() {
     initializeGameBoard();
-
     generateRandomTile();
     generateRandomTile();
-
     updateGameBoardDisplay();
-
-    score = 0; 
+    score = 0;
     updateScoreDisplay(score);
-
     document.addEventListener('keydown', handleKeyPress);
 }
-//__________________________________________________add event listner___________________________________________
+
+
 document.addEventListener('DOMContentLoaded', startGame);
 
 
